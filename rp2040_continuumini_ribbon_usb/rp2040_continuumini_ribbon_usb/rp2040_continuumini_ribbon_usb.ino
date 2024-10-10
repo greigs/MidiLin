7a800d00531773a77798798f67b2a43fbd5b58cd
greigs <>
2024-10-10 22:59:34 +0100

    combined with i2c reading joypad

--- rp2040_continuumini_ribbon_usb/rp2040_continuumini_ribbon_usb/rp2040_continuumini_ribbon_usb.ino
+++ rp2040_continuumini_ribbon_usb/rp2040_continuumini_ribbon_usb/rp2040_continuumini_ribbon_usb.ino
@@ -1,13 +1,37 @@
 #include <SPI.h>
 #include <Wire.h>
+#include "Adafruit_seesaw.h"
+#include <SPI.h>
 #include <stdlib.h>  // For random number generation
+#include <MIDI.h>
+#include "pio_usb.h"
+#include "EZ_USB_MIDI_HOST.h"
+#include "Adafruit_USBH_Host.h"
 
+// Joystick I2C settings
+#define JOY_ADDR 0x52  // Define Joystick I2C address
+#define SDA_PIN 2      // Set SDA to GPIO2
+#define SCL_PIN 3      // Set SCL to GPIO3
+
+// Gamepad button definitions
+#define BUTTON_X         6
+#define BUTTON_Y         2
+#define BUTTON_A         5
+#define BUTTON_B         1
+#define BUTTON_SELECT    0
+#define BUTTON_START    16
+
+uint32_t button_mask = (1UL << BUTTON_X) | (1UL << BUTTON_Y) | (1UL << BUTTON_START) |
+                       (1UL << BUTTON_A) | (1UL << BUTTON_B) | (1UL << BUTTON_SELECT);
+
+Adafruit_seesaw ss;
+char data[100];
+int last_x = 0, last_y = 0;
+static uint8_t x_data, y_data, button_data;
 
 int userChannel = 1;  // 1-16
 int userProgOffset = 0;
 bool serialDebug = false;
-
-#include <MIDI.h>
 
 #if defined(USE_TINYUSB_HOST) || !defined(USE_TINYUSB)
 #error "Please use the Menu to select Tools->USB Stack: Adafruit TinyUSB"
@@ -47,6 +71,8 @@ unsigned long previousMillis = 0;  // Store last time a note was sent
 const long interval = 500;         // Interval at which to send a note (500ms)
 
 int last_cc_cntrl = 1;
+
+
 
 // Function to send random MIDI note
 void sendRandomMidiNote() {
@@ -360,13 +386,30 @@ void loop1() {
 void setup() {
     TinyUSBDevice.setManufacturerDescriptor("ThereminHero");
     TinyUSBDevice.setProductDescriptor("MIDI Device");
-    if (serialDebug){
-      Serial.begin(115200);
-    }
+    
+    Serial.begin(115200);
+
     pinMode(LED_BUILTIN, OUTPUT);
     if (serialDebug){
       Serial.println("USB Host to MIDI Messenger\r\n");
     }
+
+    pinMode(SDA_PIN, INPUT_PULLUP);
+    pinMode(SCL_PIN, INPUT_PULLUP);
+    Wire.setSDA(SDA_PIN);
+    Wire.setSCL(SCL_PIN);
+    Wire.begin();
+
+    // Initialize the Adafruit seesaw gamepad
+    if (!ss.begin(0x50)) {
+        Serial.println("ERROR! seesaw not found");
+        while(1) delay(1);
+    }
+
+    // Configure button inputs on the seesaw
+    ss.pinModeBulk(button_mask, INPUT_PULLUP);
+    ss.setGPIOInterrupts(button_mask, 1);
+
     core0_booting = false;
     while (core1_booting);
 }
@@ -379,8 +422,60 @@ void loop() {
     // Tell the USB Host to send as much pending MIDI OUT data as possible
     usbhMIDI.writeFlushAll();
 
+
+
     // Do other non-USB host processing
+    delay(10);
     blinkLED();
+    
+
+
+    // Handle Joystick I2C communication
+    Wire.requestFrom(JOY_ADDR, 3);
+    if (Wire.available()) {
+        x_data = Wire.read();
+        y_data = Wire.read();
+        button_data = Wire.read();
+        // Print joystick data to the serial monitor
+        sprintf(data, "Joystick x:%d y:%d button:%d\n", x_data, y_data, button_data);
+        Serial.print(data);
+    }
+
+    // ********** Handle seesaw gamepad **********
+    // Read analog values for x and y from seesaw
+    int x = 1023 - ss.analogRead(14);  // Reverse x value
+    int y = 1023 - ss.analogRead(15);  // Reverse y value
+
+    // Print x and y values if significant change is detected
+    if ((abs(x - last_x) > 3) || (abs(y - last_y) > 3)) {
+        Serial.print("Gamepad x: "); Serial.print(x); Serial.print(", y: "); Serial.println(y);
+        last_x = x;
+        last_y = y;
+    }
+
+    // Read button states
+    uint32_t buttons = ss.digitalReadBulk(button_mask);
+
+    if (!(buttons & (1UL << BUTTON_A))) {
+        Serial.println("Button A pressed");
+    }
+    if (!(buttons & (1UL << BUTTON_B))) {
+        Serial.println("Button B pressed");
+    }
+    if (!(buttons & (1UL << BUTTON_Y))) {
+        Serial.println("Button Y pressed");
+    }
+    if (!(buttons & (1UL << BUTTON_X))) {
+        Serial.println("Button X pressed");
+    }
+    if (!(buttons & (1UL << BUTTON_SELECT))) {
+        Serial.println("Button SELECT pressed");
+    }
+    if (!(buttons & (1UL << BUTTON_START))) {
+        Serial.println("Button START pressed");
+    } 
+    
+    
 
     // Check if 200ms has passed since the last note
     unsigned long currentMillis = millis();
